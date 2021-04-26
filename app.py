49dea666c889000497bc455e5e05ca97529a6c06
Prajwal <prajwaljadhav248@gmail.com>
2021-04-27 00:20:53 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, request
+from yolo import get_predictions
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return jsonify("Object Detection")
+
+
+@app.route("/detect", methods=["GET", "POST"])
+def detect():
+    if request.method == 'POST':
+        predictions = get_predictions(request)
+        return jsonify(predictions)
+    else:
+        return jsonify("Use POST method")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5050, debug=False)
